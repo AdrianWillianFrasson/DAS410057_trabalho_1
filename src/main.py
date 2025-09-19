6c483413376cf5856497da8e6a9e7f1b42eabdea
Adrian Willian Frasson <adrianwillian.frasson@gmail.com>
2025-09-19 00:49:44 -0300

    search algorithm - A*

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-from search_algorithm import BFS, UCS
+from search_algorithm import BFS, UCS, A_star
 
-INITIAL_STATE = (
-  "bar",  # Waiter location: "bar", "table1", "table2", "table3", "table4"
-  False,  # Waiter tray: False, True
-  (),  # Waiter inventory: (("tableX", "cold"|"hot"), ...)
-  (("table1", "cold"), ("table2", "hot")),  # Orders: (("tableX", "cold"|"hot"), ...)
-  (),  # Prepared drinks: (("tableX", "cold"|"hot"), ...)
-  ("table3", "table4"),  # Tables to clean: ("tableX", ...)
-)
+
+def canonical_state(state):
+  location, tray, inventory, orders, prepared, tables_to_clean = state
+
+  return (
+    location,
+    tray,
+    tuple(sorted(inventory)),
+    tuple(sorted(orders)),
+    tuple(sorted(prepared)),
+    tuple(sorted(tables_to_clean)),
+  )
+
 
 # Distances between locations [meters]
 LOCATIONS_DISTANCE = {
@@ -40,13 +45,7 @@ def get_distance(location1, location2):
 def goal(state):
   location, tray, inventory, orders, prepared, tables_to_clean = state
 
-  return (
-    not tray
-    and len(orders) == 0
-    and len(prepared) == 0
-    and len(inventory) == 0
-    and len(tables_to_clean) == 0
-  )
+  return not tray and len(orders) == 0 and len(prepared) == 0 and len(inventory) == 0 and len(tables_to_clean) == 0
 
 
 def state_actions(state):
@@ -56,67 +55,92 @@ def state_actions(state):
   # 1. Barista can preper drinks (if there is any pending orders)
   for i, drink in enumerate(orders):
     drink_kind = drink[1]
-    cost = 3 if drink_kind == "cold" else 5
+    cost = 3.0 if drink_kind == "cold" else 5.0
     new_orders = orders[:i] + orders[i + 1 :]
     new_prepared = prepared + (drink,)
-    new_state = (location, tray, inventory, new_orders, new_prepared, tables_to_clean)
-    result.append((cost, new_state, f"prepare drink: {drink}"))
+    new_state = canonical_state((location, tray, inventory, new_orders, new_prepared, tables_to_clean))
+    result.append((cost, new_state, f"[{cost}] prepare drink: {drink}"))
 
   # 2. Waiter can walk to another location
   possible_locations = ["bar", "table1", "table2", "table3", "table4"]
   for new_location in possible_locations:
     if new_location != location:
-      speed = 1 if tray else 2
+      speed = 1.0 if tray else 2.0
       dist = get_distance(location, new_location)
       cost = dist / speed
-      new_state = (new_location, tray, inventory, orders, prepared, tables_to_clean)
-      result.append((cost, new_state, f"go: {new_location}"))
+      new_state = canonical_state((new_location, tray, inventory, orders, prepared, tables_to_clean))
+      result.append((cost, new_state, f"[{cost}] go: {new_location}"))
 
   # 3. Waiter can take or return the tray
-  if location == "bar" and not tray:
-    new_state = (location, True, inventory, orders, prepared, tables_to_clean)
-    result.append((1, new_state, "take tray"))
-  if location == "bar" and tray:
-    new_state = (location, False, inventory, orders, prepared, tables_to_clean)
-    result.append((1, new_state, "return tray"))
+  if not tray and location == "bar" and len(inventory) == 0:
+    cost = 0.0
+    new_state = canonical_state((location, True, inventory, orders, prepared, tables_to_clean))
+    result.append((cost, new_state, f"[{cost}] take tray"))
+  if tray and location == "bar" and len(inventory) == 0:
+    cost = 0.0
+    new_state = canonical_state((location, False, inventory, orders, prepared, tables_to_clean))
+    result.append((cost, new_state, f"[{cost}] return tray"))
 
   # 4. Waiter can pickup drinks from the bar without a tray
   if location == "bar" and not tray and len(inventory) == 0:
+    cost = 1.0
     for i, drink in enumerate(prepared):
       new_prepared = prepared[:i] + prepared[i + 1 :]
       new_inventory = inventory + (drink,)
-      new_state = (location, tray, new_inventory, orders, new_prepared, tables_to_clean)
-      result.append((1, new_state, f"pickup drink: {drink}"))
+      new_state = canonical_state((location, tray, new_inventory, orders, new_prepared, tables_to_clean))
+      result.append((cost, new_state, f"[{cost}] pickup drink: {drink}"))
 
   # 5. Waiter can pickup drinks from the bar with a tray
   if location == "bar" and tray and len(inventory) < 3:
-    pass
+    cost = 1.0
+    for i, drink in enumerate(prepared):
+      new_prepared = prepared[:i] + prepared[i + 1 :]
+      new_inventory = inventory + (drink,)
+      new_state = canonical_state((location, tray, new_inventory, orders, new_prepared, tables_to_clean))
+      result.append((cost, new_state, f"[{cost}] pickup drink (tray - {len(new_inventory)}): {drink}"))
 
   # 6. Waiter can deliver drinks if he is at the right table
   if len(inventory) > 0:
+    cost = 1.0
     for i, drink in enumerate(inventory):
       table, kind = drink
       if location == table:
         new_inventory = inventory[:i] + inventory[i + 1 :]
-        new_state = (location, tray, new_inventory, orders, prepared, tables_to_clean)
-        result.append((1, new_state, f"deliver drink: {drink}"))
+        new_state = canonical_state((location, tray, new_inventory, orders, prepared, tables_to_clean))
+        result.append((cost, new_state, f"[{cost}] deliver drink: {drink}"))
 
   # 7. Waiter can clean dirty tables
   if location in tables_to_clean and not tray and len(inventory) == 0:
-    cost = 4 if location == "table3" else 2
+    cost = 4.0 if location == "table3" else 2.0
     new_tables = tuple(t for t in tables_to_clean if t != location)
-    new_state = (location, tray, inventory, orders, prepared, new_tables)
-    result.append((cost, new_state, f"clean table: {location}"))
+    new_state = canonical_state((location, tray, inventory, orders, prepared, new_tables))
+    result.append((cost, new_state, f"[{cost}] clean table: {location}"))
 
   return result
 
 
 def main():
-  cost, visited, actions = UCS(INITIAL_STATE, goal, state_actions)
-  # cost, visited, actions = BFS(INITIAL_STATE, goal, state_actions)
+  initial_state = canonical_state(
+    (
+      "bar",  # Waiter location: "bar", "table1", "table2", "table3", "table4"
+      False,  # Waiter tray: False, True
+      (),  # Waiter inventory: (("tableX", "cold"|"hot"), ...)
+      (
+        ("table1", "cold"),
+        ("table1", "hot"),
+        ("table1", "hot"),
+      ),  # Orders: (("tableX", "cold"|"hot"), ...)
+      (),  # Prepared drinks: (("tableX", "cold"|"hot"), ...)
+      ("table3", "table4"),  # Tables to clean: ("tableX", ...)
+    )
+  )
 
-  print(f"Visited nodes: {len(visited)}")
-  print(f"Total Cost: {cost}")
+  cost, visited, actions = A_star(initial_state, goal, state_actions)
+  # cost, visited, actions = UCS(initial_state, goal, state_actions)
+  # cost, visited, actions = BFS(initial_state, goal, state_actions)
+
+  print(f"Number of nodes: {len(visited)}")
+  print(f"Total cost: {cost}")
 
   print("Steps:")
   for step in actions:
--- src/search_algorithm.py
+++ src/search_algorithm.py
@@ -2,31 +2,54 @@ import heapq
 from collections import deque
 
 
-# Breadth-First Search
-def BFS(initial_state, goal, state_actions):
-  queue = deque()
-  queue.append((0, initial_state, []))  # (total_cost, state, actions_steps)
+# A* Search ----------------------------------------------------------------------------------------
+def A_star(initial_state, goal, state_actions):
+  frontier = []
+  # (f = h + total_cost, total_cost, state, actions_steps)
+  heapq.heappush(frontier, (heuristic(initial_state), 0, initial_state, []))
 
-  visited = set()  # Visited nodes
+  visited = {}
 
-  while queue:
-    total_cost, state, actions = queue.popleft()
+  while frontier:
+    f, total_cost, state, actions = heapq.heappop(frontier)
 
     if goal(state):
       return total_cost, visited, actions
 
-    if state in visited:
+    if state in visited and visited[state] <= total_cost:
       continue
 
-    visited.add(state)
+    visited[state] = total_cost
 
     for new_cost, new_state, new_action in state_actions(state):
-      queue.append((total_cost + new_cost, new_state, actions + [new_action]))
+      new_total_cost = total_cost + new_cost
+      new_f = heuristic(new_state) + new_total_cost
+
+      heapq.heappush(frontier, (new_f, new_total_cost, new_state, actions + [new_action]))
 
   return None
 
 
-# Uniform-Cost Search
+def heuristic(state):
+  location, tray, inventory, orders, prepared, tables_to_clean = state
+  h = 0
+
+  # 1. Estimate prep time for pending orders
+  for _, kind in orders:
+    h += 3 if kind == "cold" else 5
+
+  # 2. Each dirty table cleaning cost
+  for table in tables_to_clean:
+    h += 4 if table == "table3" else 2
+
+  # 3. If drinks left to deliver or prepare, assume we must move at least once
+  if (orders or prepared or inventory) and location != "bar":
+    h += 1
+
+  return h
+
+
+# Uniform-Cost Search ------------------------------------------------------------------------------
 def UCS(initial_state, goal, state_actions):
   frontier = []
   heapq.heappush(frontier, (0, initial_state, []))  # (total_cost, state, actions_steps)
@@ -47,5 +70,29 @@ def UCS(initial_state, goal, state_actions):
 
     for new_cost, new_state, new_action in state_actions(state):
       heapq.heappush(frontier, (total_cost + new_cost, new_state, actions + [new_action]))
+
+  return None
+
+
+# Breadth-First Search -----------------------------------------------------------------------------
+def BFS(initial_state, goal, state_actions):
+  queue = deque()
+  queue.append((0, initial_state, []))  # (total_cost, state, actions_steps)
+
+  visited = set()  # Visited nodes
+
+  while queue:
+    total_cost, state, actions = queue.popleft()
+
+    if goal(state):
+      return total_cost, visited, actions
+
+    if state in visited:
+      continue
+
+    visited.add(state)
+
+    for new_cost, new_state, new_action in state_actions(state):
+      queue.append((total_cost + new_cost, new_state, actions + [new_action]))
 
   return None
